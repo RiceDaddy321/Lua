5ebd113561d8de578155b9a85d31e356d09de761
RiceDaddy321 <0carlitos2@gmail.com>
2020-09-03 18:09:33 -0500

    Adds a functioning UDP service through LOVE2D

---
+++ Lua-sockets/withLOVE/client/run.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+TITLE Client
+"C:\Program Files\LOVE\love.exe" "C:\Users\Juan Velazquez\Documents\LuaScripts\Lua-sockets\withLOVE\client"
+ECHO Task complete
+PAUSE
---
+++ Lua-sockets/withLOVE/server/run.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+TITLE Server
+"C:\Program Files\LOVE\love.exe" "C:\Users\Juan Velazquez\Documents\LuaScripts\Lua-sockets\withLOVE\server"
+ECHO Task complete
+PAUSE
